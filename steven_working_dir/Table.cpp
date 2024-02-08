efa502374faadf65008eda03a3ab67f2a1c5687f
GC <phchoy@connect.ust.hk>
2024-02-07 17:28:31 -0800

    add record to table

--- steven_working_dir/Table.cpp
+++ steven_working_dir/Table.cpp
@@ -3,12 +3,16 @@
 class Table;
 
 Table::Table(std::string name, int key, int num_columns) : name{name}, key{key}, num_columns{num_columns} {
-    Page** new_base_page = new Page*[num_columns + 4];
-
-    for (int i = 0; i < num_columns + 4; i++) {
-        new_base_page[i] = new Page();
-    }
+    Page* new_base_page = new Page[num_columns + 4];
+    base_pages.push_back(new_base_page);
 }
 
 Table::~Table() {}
+
+void Table::add_record(int64_t* input_data) {
+    auto final_row = base_pages.back();
+    for (int i = 0; i < num_columns; i++) {
+        final_row[i].add_record(input_data[i]);
+    }
+}
 
