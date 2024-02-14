a5ffa5d2e5f7cfa776b925de2db939d68e02a175
stevenantya <81511355+stevenantya@users.noreply.github.com>
2024-02-13 20:28:11 -0800

    move main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from table import Table
-from page import Page
+from time import process_time
+from lstore.table import Table
+from lstore.page import Page
 
 NULL_VAL = 2**64 - 1
 
 t = Table("Test", 0, 5)
 
+insert_time_0 = process_time()
 data = [1, 2, 3, 4, 5]
-for _ in range(4):
+for _ in range(10000):
     t.add_record(data)
+insert_time_1 = process_time()
+
+print("Inserting 10k records took:  \t\t\t", insert_time_1 - insert_time_0)
 
 # Assuming NULL_VAL is defined elsewhere
 udata1 = [1, NULL_VAL, NULL_VAL, 1, 1]
